c8cf51c65461d3a416b8cdca77b17319a2cdb305
ilmy17 <ilmyhusain17@gmail.com>
2023-03-30 23:24:06 +0800

    Beljar OOP PHP

---
+++ objectPegawai.php
@@ -0,0 +1,9 @@
+<?php 
+require 'Pegawai.php';
+$pegawai1 = new Pegawai('01111','Andi','Manager','Islam','Menikah');
+$ar_pegawai = [$pegawai1];
+
+foreach($ar_pegawai as $pegawai){
+    $pegawai->cetak();
+}
+?>
