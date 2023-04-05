ccc137111c324c4fbd80b75c8073eaa56adea1a3
ilmy17 <ilmyhusain17@gmail.com>
2023-04-05 22:42:15 +0800

    Upload Tugas 8 PHP native

---
+++ contact.php
@@ -0,0 +1,38 @@
+ <!-- start form -->
+ <div class="container">
+    <h2>Form Contact</h2>
+    <form action="" class="contact-form">
+        <div>
+            <label for="nama">Nama</label><br>
+            <input type="text" placeholder="Nama Lengkap" size="50" name="nama" id="nama">
+        </div>
+         
+        <div>
+            <label for="kota">Kota</label><br>
+            <input type="text" placeholder="Kota" size="50" name="kota" id="kota">
+        </div>
+        <div>   
+            <label for="email">Gmail</label><br>
+            <input type="email" placeholder="Gmail" size="50" name="email" id="email">
+        </div>
+        <div>
+            <label for="pesan">Pesan</label>
+            <br>
+            <textarea name="pesan" id="pesan" cols="50" rows="5" placeholder="Pesan"></textarea>
+        </div>
+        <div>
+            <input type="submit" name="Send" id="Send" value="Send">
+        </div>
+    </form>
+    </div>
+    <!-- end form -->
+    <!-- start menu sekunder -->
+    <div class="container-full menu-sekunder">
+        <div class="container">
+        <!-- start jang4r -->
+            <div class="menu-sekunder-one" id="contact">
+                <p>Hubungi :</p>
+                <p>Email : <a href="#" title="email">umi@gmail.com</a></p>
+            </div>
+        </div>
+    </div>
